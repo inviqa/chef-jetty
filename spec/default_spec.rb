3a83bd6d81624ef7221847cbc17a3a18b75aa65b
Kieren Evans <kevans@inviqa.com>
2015-02-17 14:47:37 +0000

    Fix centos and redhat not starting jetty even if chkconfigged to be on.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,8 +1,17 @@
-require 'chefspec'
-
 describe 'chef-jetty::default' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'chef-jetty::default' }
-  it 'should do something' do
-    pending 'Your recipe examples go here.'
+  before {
+    stub_command("test -d /usr/local/src/jetty-distribution-8.1.5.v20120716").and_return(true)
+    stub_command("test -d /usr/share/jetty/lib").and_return(true)
+    stub_command("test -f /usr/share/jetty/start.jar").and_return(true)
+    stub_command("ps ax | grep jetty | grep -v grep").and_return(false)
+  }
+
+  context 'centos' do
+    let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'centos', version: '6.5').converge 'jetty::default' }
+
+    it 'should use daemon to start jetty' do
+      expect(chef_run).to render_file('/etc/init.d/jetty').with_content('. /etc/rc.d/init.d/functions')
+      expect(chef_run).to render_file('/etc/init.d/jetty').with_content('daemon --user')
+    end
   end
 end
