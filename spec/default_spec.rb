16ff15c6c88b8974494f4c63ad5a85b4b6078611
Kieren Evans <kevans@inviqa.com>
2016-02-02 16:57:22 +0000

    Consolidate the two init.d templates for centos and redhat.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,13 +1,26 @@
-describe 'chef-jetty::default' do
-  before {
-    stub_command("test -d /usr/local/src/jetty-distribution-8.1.5.v20120716").and_return(true)
-    stub_command("test -d /usr/share/jetty/lib").and_return(true)
-    stub_command("test -f /usr/share/jetty/start.jar").and_return(true)
-    stub_command("ps ax | grep jetty | grep -v grep").and_return(false)
-  }
+describe 'jetty::default' do
+  before do
+    stub_command('test -d /usr/local/src/jetty-distribution-8.1.5.v20120716').and_return(true)
+    stub_command('test -d /usr/share/jetty/lib').and_return(true)
+    stub_command('test -f /usr/share/jetty/start.jar').and_return(true)
+    stub_command('ps ax | grep jetty | grep -v grep').and_return(false)
+  end
+
+  context 'default' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new.converge(described_recipe)
+    end
+
+    it 'should not use daemon to start jetty' do
+      expect(chef_run).to_not render_file('/etc/init.d/jetty').with_content('. /etc/rc.d/init.d/functions')
+      expect(chef_run).to_not render_file('/etc/init.d/jetty').with_content('daemon --user')
+    end
+  end
 
   context 'centos' do
-    let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'centos', version: '6.5').converge 'jetty::default' }
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'centos', version: '6.5').converge(described_recipe)
+    end
 
     it 'should use daemon to start jetty' do
       expect(chef_run).to render_file('/etc/init.d/jetty').with_content('. /etc/rc.d/init.d/functions')
